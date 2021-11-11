6d7cc8fec65173e8ff0e98ee3bf6de14e2a94c05
jisooha <haengineer12@gmail.com>
2021-11-11 17:31:46 +0900

    'step1'

---
+++ 11_ec2.tf
@@ -0,0 +1,44 @@
+/*
+data "aws_ami" "amzn" {
+    most_recent =
+
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        value = ["hvm"]
+    }
+
+    owners = ["amazon"]
+}
+*/
+
+resource "aws_instance" "jisoo_weba" {
+    ami = "ami-04e8dfc09b22389ad"
+    instance_type = "t2.micro"
+    key_name = "jisoo2-key"
+    availability_zone = "ap-northeast-2a"
+    private_ip = "10.0.0.11"
+    subnet_id = aws_subnet.jisoo_puba.id    #public_subnet a의 ID
+    vpc_security_group_ids = [aws_security_group.jisoo_sg.id]
+    user_data = <<-EOF
+                  #!/bin/bash
+                  sudo su -
+                  yum install -y httpd
+                  echo "jisoo-Terraform-1 >> /var/www/html/index.html
+                  systemctl start httpd
+                  EOF
+}
+
+resource "aws_eip" "jisoo_weba_ip" {
+    vpc = true
+    instance = aws_instance.jisoo_weba.id
+    associate_with_private_ip ="10.0.0.11"
+    depends_on = [
+        aws_internet_gateway.jisoo_ig
+    ]
+
+}
