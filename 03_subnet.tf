08b3e9ed63b6dbe09cad07d74a9656505cfb6d38
jisooha <haengineer12@gmail.com>
2021-11-12 16:16:42 +0900

    'step2'

--- 03_subnet.tf
+++ 03_subnet.tf
@@ -37,3 +37,23 @@ resource "aws_subnet" "jisoo_pric" {
     "Name" = "jisoo-pric"
   }
 }
+
+# 가용영역 a의 private DB subnet
+resource "aws_subnet" "jisoo_pridba" {
+  vpc_id = aws_vpc.jisoo_vpc.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "ap-northeast-2a"
+  tags = {
+    "Name" = "jisoo-pria"
+  }
+}
+
+# 가용영역 c의 private DB subnet
+resource "aws_subnet" "jisoo_pridbc" {
+  vpc_id = aws_vpc.jisoo_vpc.id
+  cidr_block = "10.0.5.0/24"
+  availability_zone = "ap-northeast-2c"
+  tags = {
+    "Name" = "jisoo-pric"
+  }
+}
